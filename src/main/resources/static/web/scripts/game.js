f67285069c2a929801ef2d684450c39435717328
Anton Solomin <ledorub3000@gmail.com>
2017-08-02 15:15:26 +0300

    Fixed several bugs of ship placement.
    - can not place ships on the already occupied spot (spot goes red)
    - can not go over the board when placing (ship goes red)
    - js requires all the ships to be placed before sending
    - js alerts if a user is trying to send an incomplete ship map

--- src/main/resources/static/web/scripts/game.js
+++ src/main/resources/static/web/scripts/game.js
@@ -1,5 +1,6 @@
 $(function () {
 	$("#submitlogout").click(logOutRedirect);
+	$("#redirectToGames").click(toGames);
 	//getting correct link
 	var queryObj = parseQueryObject();
 	if (queryObj.hasOwnProperty("gp")) {
@@ -10,6 +11,17 @@ $(function () {
 });
 
 function onDataReady(data) {
+	//hiding the enemy map until we submit the ships 
+	$("#salvosMap").hide();
+	//after reload if ships have been placed we enter here
+	if (data.ships.length !== 0) {
+		$("#salvosMap").show();
+		$("#submitShips").hide();
+		$("#boardClear").hide();
+		$(".ship").hide();
+		$("#message").html("Prepare for battle");
+	}
+
 	console.log(data);
 	renderTables(1);
 	renderTables(2);
@@ -17,21 +29,25 @@ function onDataReady(data) {
 	renderSalvos(data, "#salvosMap");
 	renderSalvos(data, "#yourShipsMap");
 	renderPlayerInfo(data);
-	$("#salvosMap").hide();
-	$("td[data-length]").click(choseShip);
 
+	$("td[data-length]").click(choseShip);
 	$("td[data-location1]").click(placeShipsOnTheMap);
 	$("td[data-location1]").mouseover(hoverHighlight);
 	$("#submitShips").click(sendShips);
-	//$("#boardClear").click(clearTheBoard);
+	$("#boardClear").click(clearTheBoard);
 }
 
+//to be sent to the back end
 var shipLength = 0;
 var shipClass = "";
+var shipClassElement;
 var locationsArr = [];
 var shipsArr = [];
-
 var arr = [];
+
+function toGames() {
+	location.assign("/web/games.html");
+}
 
 function clearTheBoard() {
 	shipLength = 0;
@@ -44,11 +60,13 @@ function clearTheBoard() {
 }
 
 function hoverHighlight() {
+	// remove coloring classes and clear arr on a new hover
 	for (var i = 0; i < arr.length; i++) {
 		arr[i].removeClass("highlight");
+		arr[i].removeClass("overlap");
 	}
 	arr = [];
-	//$(this).css("background-color", "black");
+
 	var alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 	var current = $(this).attr("data-location1");
 
@@ -58,57 +76,109 @@ function hoverHighlight() {
 	for (var i = 0; i < alphabet.length; ++i) {
 		if (alphabet[i] == currentLetter[0]) {
 			var nextLetter = alphabet.slice(i);
+			var test = [];
 			for (var j = 0; j < shipLength; ++j) {
 				var letter = nextLetter[j].slice(0, 1);
-				$("td[data-location1='" + letter + currentNumber + "']").addClass("highlight");
-				arr.push($("td[data-location1='" + letter + currentNumber + "']"));
+				var location = letter + currentNumber;
+				$("td[data-location1='" + location + "']").addClass("highlight");
+				// add to an arr to remember what to clear
+				arr.push($("td[data-location1='" + location + "']"));
+				// adding locations to test arr so we can paind whole ships red later if they go overedge
+				test.push(location);
+				if (isOverlap(location)) {
+					// add to an arr to remember what to clear
+					arr.push($("td[data-location1='" + location + "']").addClass("overlap"));
+				}
+				
+			}
+			// printing whole ships red if they go overedge
+			for (var u = 0;u< test.length; u++) {
+				if (isOverEdge(test[u])) {
+					for (var l = 0;l< test.length; l++) {
+						$("td[data-location1='" + test[l] + "']").addClass("overlap");
+					}
+				}
 			}
 		}
 	}
+}
+
+function isOverlap(toCheck) {
+	for (var i = 0; i < shipsArr.length; i++) {
+		for (var j = 0; j < shipsArr[i].locations.length; j++) {
+			if (shipsArr[i].locations[j] == toCheck) {
+				return true;
+			}
+		}
+	}
+	return false
+}
+
+function isOverEdge(toCheck) {
+	var alphabet = "KLMNOPQRSTUVWXYZ".split("");
+	var letter = toCheck.split("");
+	for (var i = 0; i < alphabet.length; ++i) {
+		if (alphabet[i] == letter[0]) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function choseShip() {
 	shipLength = $(this).attr("data-length");
 	shipClass = $(this).parent().attr("id");
 	shipClassElement = $(this).parent();
-	console.log("Ship length is: " + shipLength);
-	console.log(shipClassElement);
 }
 
 function placeShipsOnTheMap() {
-
+	console.log(locationsArr);
 	var alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
 	//getting the start location
 	var start = $(this).attr("data-location1");
-	console.log();
-
 	// getting the number and the letter of the selected start location
 	var startLetter = start.split("");
 	var startNumber = start.slice(1);
-
 	// horisontal placement logic
 	for (var i = 0; i < alphabet.length; ++i) {
 		if (alphabet[i] == startLetter[0]) {
 			var nextLetter = alphabet.slice(i);
+			//clearing locations arr
 			locationsArr = [];
-			addtoShipsObj();
-			shipClassElement.addClass("hide");
+			var cellsToBeColored = [];
 			for (var j = 0; j < shipLength; ++j) {
 				var letter = nextLetter[j].slice(0, 1);
 				var location = (letter + startNumber);
-				paintShipOnTheMap(location);
-				locationsArr.push(location);
+				//if there's an overlap return false
+				if (isOverlap(location)) {
+					console.log("You may not place ships on top of one another");
+					return false;
+				} else if (isOverEdge(location)) {
+					console.log("You may not place ships over the edge of the map");
+					return false;
+				} else {
+					cellsToBeColored.push(location);
+					//adding to locations arr(for ships obj)
+					locationsArr.push(location);
+				}
 			}
+			//painting ship locations
+			for (var p = 0; p<cellsToBeColored.length ; p++) {
+				paintShipOnTheMap(cellsToBeColored[p]);
+			}
+			//hiding from selection
+			shipClassElement.addClass("hide");
+			// adding to ships obj
+			addtoShipsObj();
+			//clearing the highlight after a ship's been placed
+			shipLength = 0;
+			shipClass = "";
 		}
 	}
 }
 
-function paintShipOnTheMap(location) {
-	$("td[data-location1='" + location + "']").addClass("placed");
-}
-
 function addtoShipsObj() {
+	//creating a ships obj and adding it to ships arr(that will be sent to back)
 	var ship = {};
 	ship.locations = locationsArr;
 	ship.shipClass = shipClass;
@@ -119,38 +189,31 @@ function addtoShipsObj() {
 	console.log(shipsArr);
 }
 
-function test() {
-	console.log("Test");
-	console.log(shipsArr);
+function paintShipOnTheMap(location) {
+	if (!isOverlap(location)) {
+		$("td[data-location1='" + location + "']").addClass("placed");
+	}
 }
 
 function sendShips() {
+	console.log("What you send is : " + shipsArr);
 	var queryObj = parseQueryObject();
-	console.log("api/games/players/" + queryObj.gp + "/ships");
-	console.log(shipsArr);
-
-	$.post({
+	if(shipsArr.length === 5) {
+		$.post({
 		url: "/api/games/players/" + queryObj.gp + "/ships",
 		data: JSON.stringify(shipsArr),
 		dataType: "json",
 		contentType: "application/json"
 	}).done(function () {
-		$("#salvosMap").show();
-		$("#submitShips").hide();
-		$("#boardClear").hide();
-		$("#message").html("Prepare for battle");
-		
+		location.reload();
 		console.log("Success!");
 	}).fail(function () {
 		console.log("Fail!");
 	});
-}
-
-function getGamePlayerId() {
-	var queryObj = parseQueryObject();
-	if (queryObj.hasOwnProperty("gp")) {
-		return queryObj.gp;
+	} else {
+		alert("You must place all the ships on the board");
 	}
+	
 }
 
 function renderTables(str) {
@@ -179,12 +242,13 @@ function renderTables(str) {
 }
 
 function renderShips(data, tableSelector) {
+	//console.log("Im trying to render ships");
 	var $map = $(tableSelector).find(".fields");
 	for (var i = 0; i < data.ships.length; ++i) {
 		for (var k = 0; k < data.ships[i].locations.length; ++k) {
 			for (var j = 0; j < $map.length; ++j) {
 				var $field = $($map[j]);
-				if (data.ships[i].locations[k] == $field.attr("data-location")) {
+				if (data.ships[i].locations[k] == $field.attr("data-location1")) {
 					$field.css("background-color", "green");
 				}
 			}
@@ -227,7 +291,7 @@ function renderSalvos(data, tableSelector) {
 				for (var i = 0; i < mySalvoTurn.length; ++i) {
 					for (var j = 0; j < $myMap.length; ++j) {
 						var $field = $($myMap[j]);
-						if (mySalvoTurn[i] == $field.attr("data-location")) {
+						if (mySalvoTurn[i] == $field.attr("data-location2")) {
 							$field.html("<p class='hit'>" + turnKey + "</p>");
 						}
 					}
@@ -244,7 +308,7 @@ function renderSalvos(data, tableSelector) {
 				for (var i = 0; i < enemySalvoTurn.length; ++i) {
 					for (var j = 0; j < $enemyMap.length; ++j) {
 						var $enemyField = $($enemyMap[j]);
-						if (enemySalvoTurn[i] == $enemyField.attr("data-location")) {
+						if (enemySalvoTurn[i] == $enemyField.attr("data-location1")) {
 							$enemyField.html("<p class='hit'>" + enemyTurnKey + "</p>");
 						}
 					}
